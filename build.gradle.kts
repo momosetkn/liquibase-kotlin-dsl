5592eff19049485fe694ebe7dad4885024bc807f
t.momose <hyakkun@gmail.com>
2024-08-05 23:34:01 +0900

    Update gradle. Change to Kotlin.

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+val kotestVersion: String by project
+
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "innoscouter"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // test
+    testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-framework-engine-jvm:$kotestVersion")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+
+    // db-migration
+    implementation("org.liquibase:liquibase-core:4.27.0")
+    implementation("info.picocli:picocli:4.7.6")
+}
